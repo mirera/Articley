5ed22b9032d5ec9414204903f6706747112fd624
Mirera Moses <mirera.moses@gmail.com>
2019-02-25 17:08:41 +0300

    [chore#100002]configuring and initiliazing MySQL

--- myflaskapp/app/app.py
+++ myflaskapp/app/app.py
@@ -5,6 +5,14 @@ from wtforms import Form,StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
+#config MYSQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'Whitemirera123@'
+app.config['MYSQL_DB'] = 'Articley'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+#init MYSQL
+mysql = MySQL(app)
 
 Articles = Articles()
 @app.route('/')
@@ -43,7 +51,7 @@ class RegisterForm(Form):
         validators.Length(min=6, max=50)
         
     ])
-    
+
     password = PasswordField('Password', [
         validators.DataRequired(),
         validators.EqualTo('confirm', message='Passwords do not match')
@@ -55,9 +63,29 @@ class RegisterForm(Form):
 def register():
     form = RegisterForm(request.form)
     if request.method == 'POST' and form.validate():
-        return render_template('register.html')
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+
+        #Create cursor
+        cur = mysql.connection.cursor()
+
+        # Excute query
+        cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
+
+        #commit to DB
+        mysql.connection.commit()
+
+        #Close connection
+        cur.close()
+
+        flash('You are now registered and can log in', 'success' )
+
+        redirect(url_for('index'))
 
     return render_template('register.html', form=form)
 
 if __name__ == '__main__':
+    app.secret_key='secret123'
     app.run(debug = True)
